fixed quaternions

traj_opt/fixed_contact_point_system.py
@@ -11,8 +11,6 @@ class FixedContactPointSystem:
                nGrid     = 100,
                dt        = 0.1,
                cp_params = None,
-               platform  = None,
-               obj_pose  = None,
                obj_shape = None,
                obj_mass  = None,
                log_file  = None,
@@ -32,7 +30,6 @@ class FixedContactPointSystem:
 
     self.p = 100
 
-    self.obj_pose = obj_pose
     self.obj_shape = obj_shape # (width, length, height), (x, y, z)
     self.obj_mass = obj_mass
     self.obj_mu = 1
@@ -54,8 +51,6 @@ class FixedContactPointSystem:
     self.H = np.zeros((l_i*self.fnum,self.obj_dof*self.fnum))
     for i in range(self.fnum):
       self.H[i*l_i:i*l_i+l_i, i*self.obj_dof:i*self.obj_dof+self.obj_dof] = H_i
-
-    self.platform = platform 
 
     self.log_file = log_file
 
@@ -326,7 +321,7 @@ class FixedContactPointSystem:
   """
   def get_grasp_matrix(self, x):
     # Transformation matrix from object frame to world frame
-    quat_o_2_w = [x[0,4], x[0,5], x[0,6], x[0,3]]
+    quat_o_2_w = [x[0,3], x[0,4], x[0,5], x[0,6]]
 
     G_list = []
 
@@ -402,7 +397,7 @@ class FixedContactPointSystem:
     #return H
 
   def get_R_o_2_w(self, x):
-    quat = [x[0,4], x[0,5], x[0,6], x[0,3]]
+    quat = [x[0,3], x[0,4], x[0,5], x[0,6]]
     R = utils.get_matrix_from_quaternion(quat)
     return R
 
@@ -414,7 +409,7 @@ class FixedContactPointSystem:
   def get_H_o_2_w(self, x):
     H = SX.zeros((4,4))
     
-    quat = [x[0,4], x[0,5], x[0,6], x[0,3]]
+    quat = [x[0,3], x[0,4], x[0,5], x[0,6]]
     R = utils.get_matrix_from_quaternion(quat)
     p = np.array([x[0,0], x[0,1], x[0,2]])
 
@@ -431,7 +426,7 @@ class FixedContactPointSystem:
   """
   def get_H_w_2_o(self, x):
     H = np.zeros((4,4))
-    quat = [x[0,4], x[0,5], x[0,6], x[0,3]]
+    quat = [x[0,3], x[0,4], x[0,5], x[0,6]]
     p = np.array([x[0,0], x[0,1], x[0,2]])
     p_inv, quat_inv = utils.invert_transform(p, quat)
     R = utils.get_matrix_from_quaternion(quat_inv)
@@ -586,7 +581,7 @@ class FixedContactPointSystem:
     x_range = np.array([
                        [-5,5], # x coord range
                        [-5,5], # y coord range
-                       [-5,5], # z coord range
+                       [0.0325,5], # z coord range
                        [-np.inf, np.inf], # qw range
                        [-np.inf, np.inf], # qx range
                        [-np.inf, np.inf], # qx range

traj_opt/test_traj_opt.py
@@ -13,22 +13,27 @@ from rrc_simulation import trifinger_platform, sample
 from rrc_simulation.tasks import move_cube
 from fixed_contact_point_opt import FixedContactPointOpt
 
-x_goal = np.array([[0,0,0.1+0.0325,0,0,0,1]])
+#x_goal = np.array([[0.08,0.08,0.08 + 0.0325,0,0,0,1]]) # pushing
+x_goal = np.array([[0,0,0.1+0.0325,0,0,0,1]]) # 10 cm lift
 x_goal_str = "-".join(map(str,x_goal[0,:].tolist()))
+x0        = np.array([[0,0,0.0325,0,0,0,1]])
+x0_str = "-".join(map(str,x0[0,:].tolist()))
 
-nGrid = 100
+nGrid = 150
 dt = 0.1
 
 # Contact point position parameters
 # 1 finger on face 5, 1 finger on face 3
 cp_params = [
-             [0, 1, 0],
+             #[0, 1, 0],
+             [1, 0, 0],
              [0, -1, 0],
              [-1, 0, 0],
             ]
 #cp_params = [
-#             [0, 0, 1],
-#             [0, 0, 1],
+#             [0, 1, 0],
+#             [0, -1, 0],
+#             [-1, 0, 0.5],
 #            ]
 
 # Files to save solutions
@@ -37,7 +42,7 @@ save_dir = "./logs/{}".format(today_date)
 # Create directory if it does not exist
 if not os.path.exists(save_dir):
   os.makedirs(save_dir)
-save_string = "{}/fixed_cp_object_xgoal_{}_nGrid_{}_dt_{}".format(save_dir, x_goal_str, nGrid, dt) 
+save_string = "{}/fixed_cp_object_x0_{}_xgoal_{}_nGrid_{}_dt_{}".format(save_dir, x0_str, x_goal_str, nGrid, dt) 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -83,9 +88,9 @@ def main():
                                       nGrid     = nGrid, # Number of timesteps
                                       dt        = dt,   # Length of each timestep (seconds)
                                       cp_params = cp_params,
+                                      x0        = x0,
                                       x_goal    = x_goal,
                                       platform  = platform,
-                                      obj_pose  = cube_pose,
                                       obj_shape = cube_shape,
                                       obj_mass  = cube_mass,
                                       )
@@ -93,6 +98,7 @@ def main():
     # Save solution in npz file
     np.savez(save_string,
              dt         = opt_problem.dt,
+             x0         = x0,
              x_goal     = x_goal,
              t          = opt_problem.t_soln,
              x          = opt_problem.x_soln,